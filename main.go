dfba528bd5d134129dcfb3ab91fbedf7ecbbfa79
H4ckit <dammysteve10@gmail.com>
2022-01-16 00:16:05 +0100

    Changed Test String To Test For A New Edge Case

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	//	text := "1466-boi-3784-guw-7113-rqgw-926-bht-8681-e-7143"
-	text := "20-ab-50-cd-99-df"
+	//	text := "20-ab-50-cd-99-df"
+	text := "lkhgmr2eg6-xhacqdwh1cf-mkilq9op0kj7193h9qx2cj"
 	fmt.Println(assessment.TestValidity(text))
-	fmt.Println(assessment.StoryStats(text))
+	//	fmt.Println(assessment.StoryStats(text))
 	/*        fmt.Println(averageNumber(text))
 	          fmt.Println(wholeStory(text))
 	          fmt.Println(storyStats(text))
