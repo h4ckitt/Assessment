8cdd4fbb70e56eb47c3d5030451f6c1c7d76e6d9
H4ckit <dammysteve10@gmail.com>
2022-01-16 00:42:06 +0100

    Represents Each Function In A Cleaner Format

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"assessment/assessment"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	//	text := "1466-boi-3784-guw-7113-rqgw-926-bht-8681-e-7143"
-	//	text := "20-ab-50-cd-99-df"
-	text := "lkhgmr2eg6-xhacqdwh1cf-mkilq9op0kj7193h9qx2cj"
-	fmt.Println(assessment.TestValidity(text))
-	//	fmt.Println(assessment.StoryStats(text))
-	/*        fmt.Println(averageNumber(text))
-	          fmt.Println(wholeStory(text))
-	          fmt.Println(storyStats(text))
-	          fmt.Println(generate(true))
-	          fmt.Println(generate(false))
-	*/
+	text := "1466-boi-3784-guw-7113-rqgw-926-bht-8681-e-7143-i"
+	//text := "lkhgmr2eg6-xhacqdwh1cf-mkilq9op0kj7193h9qx2cj"
+
+	fmt.Println("Text Is Valid: ", assessment.TestValidity(text))
+	shortest, longest, average, result := assessment.StoryStats(text)
+	fmt.Printf("Shortest Word: %s\nLongest Word: %s\nAverage WordLength: %f\nResult: %s\n", shortest, longest, average, strings.Join(result, ","))
+	fmt.Println("Whole Story: ", assessment.WholeStory(text))
+	fmt.Println("Average Number: ", assessment.AverageNumber(text))
+	fmt.Println("Valid Generated Text: ", assessment.Generate(true))
+	fmt.Println("Invalid Generated Text: ", assessment.Generate(false))
+
 }
